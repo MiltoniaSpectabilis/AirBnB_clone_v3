f7556b93f82c4b65e82d2b47dbc7094894464fab
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-07 02:41:06 +0100

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask Application"""
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def close_db(error):
     """Close Storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 Error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
