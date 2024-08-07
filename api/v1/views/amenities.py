4b5a24dc9c272f89be5b37078ed675751c1da6ee
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-07 03:22:50 +0100

    pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -14,7 +14,8 @@ def get_amenities():
     return jsonify(list_amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieves an Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -48,7 +49,8 @@ def create_amenity():
     return make_response(jsonify(instance.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     """Updates an Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
