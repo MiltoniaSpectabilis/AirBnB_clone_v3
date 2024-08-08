26aabbfc301877bd888d628ecf5a35f4349152a5
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-08 02:04:03 +0100

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,27 +2,19 @@
 """
 Flask Application
 """
-from flask import Flask, jsonify, make_response
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
 def close_db(error):
     """Close Storage"""
     storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """404 Error"""
-    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
