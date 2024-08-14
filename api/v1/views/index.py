65f32d3431b3cd735431b720ae030891027eb3ef
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-14 09:50:29 +0100

    add doc for index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines the /status route.
+"""
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status')
 def status():
-    """Status of the API"""
+    """
+    Returns the status of the API.
+    """
     return jsonify({"status": "OK"})
 # #!/usr/bin/python3
 # """
